f05350016106e0c4afb5fb5ae289c305107ca5e1
gardrh <gardrh@gmail.com>
2023-02-23 15:26:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const generateBtn = document.getElementById("generate-btn");
+const resetBtn = document.getElementById("reset-btn");
+const filterSelect = document.getElementById("filter-select");
+const resultContainer = document.getElementById("result-container");
+const data = [
+  { name: "Vegetar", variable: "Vegetar" },
+  { name: "Kjøtt", variable: "Kjøtt" },
+  { name: "Fisk", variable: "Fisk" },
+
+];
+
+function generateRandomResult() {
+  let filteredData = data;
+  if (filterSelect.value !== "") {
+    filteredData = data.filter(option => option.variable === filterSelect.value);
+  }
+  // code to generate random result from filteredData
+  resultContainer.textContent = "Random result goes here";
+}
+
+function resetResult() {
+  resultContainer.textContent = "";
+}
+
+generateBtn.addEventListener("click", generateRandomResult);
+resetBtn.addEventListener("click", resetResult);
+filterSelect.addEventListener("change", generateRandomResult);
