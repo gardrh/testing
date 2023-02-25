5079fe4c4e611ea14a37ad23d4d80bde99395c24
gardrh <gardrh@gmail.com>
2023-02-25 13:58:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,16 @@
 const generateBtn = document.getElementById("generate-btn");
-const resetBtn = document.getElementById("reset-btn");
-const filterSelect = document.getElementById("filter-select");
-const resultContainer = document.getElementById("result-container");
-let data = [];
+const resultDiv = document.querySelector(".result");
 
-function generateRandomResult() {
-  var filter = document.getElementById("filter-select").value;
-  var filteredData = data;
-  if (filter !== "") {
-    filteredData = data.filter(function (row) {
-      return row.Type === filter;
-    });
-  }
-  var randomIndex = Math.floor(Math.random() * filteredData.length);
-  var randomItem = filteredData[randomIndex];
-  var resultContainer = document.getElementById("result-container");
-  resultContainer.innerHTML = randomItem.Name;
-}
-
-}
-
-function resetResult() {
-  resultContainer.textContent = "";
-}
-
-generateBtn.addEventListener("click", generateRandomResult);
-resetBtn.addEventListener("click", resetResult);
-filterSelect.addEventListener("change", generateRandomResult);
-
-fetch("data.csv")
-  .then(response => response.text())
-  .then(data => {
-    const rows = data.trim().split('\n');
-    const headers = rows.shift().split(',');
-    data = rows.map(row => {
-      const values = row.split(',');
-      const obj = {};
-      headers.forEach((header, index) => {
-        obj[header] = values[index];
-      });
-      return obj;
-    });
-  })
-  .catch(error => console.log(error));
+generateBtn.addEventListener("click", () => {
+  fetch("data.csv")
+    .then(response => response.text())
+    .then(text => {
+      const data = text.trim().split("\n").map(row => row.split(","));
+      const headers = data.shift();
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const randomRow = data[randomIndex];
+      const resultHTML = headers.map((header, index) => `<p><strong>${header}:</strong> ${randomRow[index]}</p>`).join("");
+      resultDiv.innerHTML = resultHTML;
+    })
+    .catch(error => console.error(error));
+});
