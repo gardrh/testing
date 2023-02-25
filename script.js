b4e8573139c05071510585f91a1ab878512cf6b7
gardrh <gardrh@gmail.com>
2023-02-25 15:11:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,16 +16,16 @@ generateBtn.addEventListener("click", () => {
 });
 
 
-// Load the data from the CSV file
-d3.csv("data.csv").then(function(data) {
-  // Calculate the total number of dinners
-  var dinnerCount = 0;
-  data.forEach(function(row) {
-    dinnerCount += parseInt(row.Antall);
-  });
-
-  // Update the element with the dinner count
-  var dinnerCountElement = document.getElementById("dinner-count");
-  dinnerCountElement.innerHTML = "Total number of dinners: " + dinnerCount;
-});
-
+// Fetch the data.csv file
+fetch('data.csv')
+  .then(response => response.text())
+  .then(data => {
+    // Split the data into an array of rows
+    const rows = data.trim().split('\n');
+    // Count the number of rows (subtracting 1 for the header row)
+    const count = rows.length - 1;
+    // Update the dinner-count element with the count
+    const dinnerCountElement = document.getElementById('dinner-count');
+    dinnerCountElement.textContent = `There are ${count} dinners to choose from.`;
+  })
+  .catch(error => console.error(error));
